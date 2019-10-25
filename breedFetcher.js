05f27c42180d8d055eed1fcb5511a10f52145eff
ZSully09 <zsullivan93@gmail.com>
2019-10-25 01:52:26 +0000

    refactoring commit

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,17 @@ const request = require("request");
 // Require File System module
 // const fs = require("fs");
 // Create variable to pass the given URL
-const url = process.argv[2];
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log("error:", error); // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  // console.log(typeof body);
-  // console.log("body:", body); // Print the HTML for the Example homepage.
-  // // Write the body of www.example.com/ into our local index.html file; if error, throw error
-  // console.log(data);
-  // console.log(typeof data);
-  console.log("data:", data[0].description);
+const url = "https://api.thecatapi.com/v1/breeds/search?q=";
+const breed = process.argv[2];
+request(url + breed, (error, response, body) => {
+  if (error) {
+    console.log("error:", error);
+  } else if (body) {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("Breed not found");
+    } else {
+      console.log(data[0].description);
+    }
+  }
 });
