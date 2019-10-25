973b2645daffb51afebb40290543553f8167f1c2
ZSully09 <zsullivan93@gmail.com>
2019-10-25 03:49:40 +0000

    mocha and chai test

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,16 +4,23 @@ const request = require("request");
 // const fs = require("fs");
 // Create variable to pass the given URL
 const url = "https://api.thecatapi.com/v1/breeds/search?q=";
-const breed = process.argv[2];
-request(url + breed, (error, response, body) => {
-  if (error) {
-    console.log("error:", error);
-  } else if (body) {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log("Breed not found");
-    } else {
-      console.log(data[0].description);
+
+const fetchBreedDescription = (breed, callback) => {
+  request(url + breed, (error, response, body) => {
+    if (error) {
+      callback(error);
+      // console.log("error:", error);
+    } else if (body) {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback(null);
+        // console.log("Breed not found");
+      } else {
+        callback(null, data[0].description);
+        // console.log(data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
